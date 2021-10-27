5ce439463eed5857361072958c578d4b95586242
zhang-glitch <669638556@qq.com>
2021-10-27 15:34:41 +0800

    feat(hello): 删除hello组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo"
-         src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
@@ -10,22 +8,10 @@
 import { defineComponent } from 'vue'
 
 
-
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
 export default defineComponent({
   name: 'Home',
 
-
-
   components: {
-    HelloWorld
-
-
-
   }
-
-
-  
 })
 </script>
